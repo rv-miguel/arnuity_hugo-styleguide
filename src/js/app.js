d34c3478770205416f99f90c293915f03227732d
Miguel <mfondeur@redventures.com>
2017-08-23 19:16:11 -0400

    remove code to solve error

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-// JS Goes here - ES6 supported
-Array.from(document.querySelectorAll(".styleguide-example-toggle")).forEach((el) => {
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
-    el.parentElement.classList.toggle("styleguide-example-collapsed");
-  }, false);
-});
